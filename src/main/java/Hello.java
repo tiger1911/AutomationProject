369f08c94abf6bddf3762594e63ac4bed96a11f5
tiger1911 <mehmetatas@yahoo.com>
2020-09-30 22:43:53 +0200

    EU3 is doing great! again

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
         //i am ready to commit this
 
         // EU3 IS AWESOME!!!
-        //EU3 IS LEARNING HOW TO USE GIT
+        //EU3 IS LEARNING HOW TO USE GIT FROM INTELLIJ
     }
 
 }
